50a99823d502a3a5d367fb7a32e85226181eaafe
petromath77 <petromath77@gmail.com>
2019-03-02 22:13:11 +0300

    2page components

--- source/index.js
+++
@@ -1,3 +0,0 @@
-import createMenu from './menu';
-let menu = createMenu(['Portfolio', 'My Works', 'About Me'], 'menu');
-document.body.appendChild(menu);
---
+++ source/pages/index/index.js
@@ -0,0 +1,3 @@
+import createMenu from '../../components/menu/menu';
+let menu = createMenu(['Index', 'Blog'], 'index');
+document.body.appendChild(menu);
